d45625e856e95e23d9ddb8491a7fdfb5718fa12a
Daniel Mason <danielboeker@iinet.net.au>
2018-02-15 19:51:59 +0800

    added data, changed heading

--- rebuild.sh
+++ rebuild.sh
@@ -2,11 +2,10 @@ git pull
 docker build -t danobot/suggestive-giraffe:rpi3 .
 docker push danobot/suggestive-giraffe:rpi3
 echo "$(date)    Built image on Raspberry Pi" >> builds.log
-git add builds.log
-git commit -m "Appended build record to build log"
+git commit ./builds.log -m "Appended build record to build log"
 git push
 
-#cd ~/dockerfiles/master/
-#docker stop ha-api-tool-prod
-#docker rm ha-api-tool-prod
-#docker-compose up -d ha-api-tool
+cd ~/dockerfiles/master/
+docker stop suggestive-giraffe-demo
+docker rm  suggestive-giraffe-demo
+docker-compose up -d  giraffe-demo
